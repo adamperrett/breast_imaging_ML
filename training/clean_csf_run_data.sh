2b920324edb153d21afc66254be57658d764d1aa
adamgoodtime <adam.perrett@manchester.ac.uk>
2024-04-25 15:57:42 +0100

    cleaner cleaning

--- training/clean_csf_run_data.sh
+++ training/clean_csf_run_data.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
 qdel submit_opt*
-rm submit_optuna_regression_job.*
 rm *.db
 rm -rf results/*
 rm ../models/l_*
 rm ../models/r_*
+rm submit_optuna_regression_job.*
+echo ""
 echo "remember to stop the job submitting script"
+echo ""
