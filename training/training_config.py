bf613cc740ff14fbd8ad4d058b83940558fb1d83
adamgoodtime <adam.perrett@manchester.ac.uk>
2024-06-12 15:59:58 +0100

    correct config for the csf

--- training/training_config.py
+++ training/training_config.py
@@ -30,68 +30,40 @@ on_CSF = True
 optuna_optimisation = False
 
 if on_CSF:
-    '''
-    Test across:
-    -n images 4
-    -dataset 4
-    -batch_size 6
-    -optimiser 2
-    -weights 2
-    -transformed 2
-    '''
     working_dir = '/mnt/iusers01/gb01/mbaxrap7/scratch/breast_imaging_ML/training/'
     # working_dir = 'C:/Users/adam_/PycharmProjects/breast_imaging_ML/training/'
-    if optuna_optimisation:
-        base_name = 'vas_baseline'
-        n_images = 0
-        # processed_dataset_path = '/mnt/bmh01-rds/assure/processed_data/'
-        processed_dataset_path = '/mnt/iusers01/gb01/mbaxrap7/scratch/breast_imaging_ML/processed_data/'
-        # processed_dataset_path = 'C:/Users/adam_/PycharmProjects/breast_imaging_ML/processed_data/'
-        # data_name = 'procas_pvas_vbd_processed_per_im_base'
-        # data_name = 'local_pvas_vas_raw_base'
-        data_name = 'procas_pvas_vas_raw_base'
-        priors_name = 'priors_pvas_vas_raw_base'
-        if split_CC_and_MLO:
-            base_name += '_' + CC_or_MLO
-            data_name += '_' + CC_or_MLO
-            priors_name += '_' + CC_or_MLO
-        processed_priors_path = processed_dataset_path+priors_name+'.pth'
-        processed_dataset_path += data_name+'.pth'
-
-        # data_name = 'priors_pvas_processed_base_CC'
-        # processed_dataset_path = 'C:/Users/adam_/PycharmProjects/breast_imaging_ML/processed_data/priors_pvas_processed_base_CC.pth'
-    else:
-        configurations = []
-        for b_size in [512, 256, 128, 64]:
-            for op_choice in ['adam', 'sgd', 'd_adam', 'd_sgd']:
-                for weight_choice in [0, 1]:
-                    for trans_choice in [0, 1]:
-                        configurations.append({
-                            'lr': lr,
-                            'batch_size': b_size,
-                            'optimizer': op_choice,
-                            'weighted': weight_choice,
-                            'transformed': trans_choice
-                        })
-        best_model_name = 'VAS_csf_{}_{}x{}_t{}_w{}_{}'.format(
-            op_choice, batch_size, lr, transformed, weighted, int(sys.argv[1]))
-
-        print("Config", int(sys.argv[1]) + 1, "creates test", best_model_name)
+    base_name = 'vas_baseline'
+    n_images = 0
+    # processed_dataset_path = '/mnt/bmh01-rds/assure/processed_data/'
+    processed_dataset_path = '/mnt/iusers01/gb01/mbaxrap7/scratch/breast_imaging_ML/processed_data/'
+    # processed_dataset_path = 'C:/Users/adam_/PycharmProjects/breast_imaging_ML/processed_data/'
+    # data_name = 'procas_pvas_vbd_processed_per_im_base'
+    # data_name = 'local_pvas_vas_raw_base'
+    data_name = 'procas_pvas_vas_raw_base'
+    priors_name = 'priors_pvas_vas_raw_base'
+    if split_CC_and_MLO:
+        base_name += '_' + CC_or_MLO
+        data_name += '_' + CC_or_MLO
+        priors_name += '_' + CC_or_MLO
+    processed_priors_path = processed_dataset_path+priors_name+'.pth'
+    processed_dataset_path += data_name+'.pth'
 else:
 
-    n_images = 8
 
-    image_directory = 'D:/mosaic_data/raw'
-    csv_directory = 'C:/Users/adam_/PycharmProjects/breast-cancer/data'
-    csv_name = 'full_procas_info3.csv'
-    reference_csv = 'PROCAS_reference.csv'
-
-    keyword = 'local_testing'
-    dataset = 'log'
-
-    # processed_dataset_path = os.path.join(csv_directory, 'mosaics_processed/mosaic_pvas_dataset_{}.pth'.format(dataset))
-    # processed_dataset_path = os.path.join(csv_directory, 'mosaics_processed/full_mosaic_dataset_log.pth')
-
+    # working_dir = '/mnt/iusers01/gb01/mbaxrap7/scratch/breast_imaging_ML/training/'
     working_dir = 'C:/Users/adam_/PycharmProjects/breast_imaging_ML/training/'
-    best_model_name = '{}_{}_{}_{}x{}_t{}_w{}'.format(
-        keyword, dataset, op_choice, batch_size, n_images, transformed, weighted)
+    base_name = 'vas_baseline'
+    n_images = 0
+    # processed_dataset_path = '/mnt/bmh01-rds/assure/processed_data/'
+    # processed_dataset_path = '/mnt/iusers01/gb01/mbaxrap7/scratch/breast_imaging_ML/processed_data/'
+    processed_dataset_path = 'C:/Users/adam_/PycharmProjects/breast_imaging_ML/processed_data/'
+    # data_name = 'procas_pvas_vbd_processed_per_im_base'
+    data_name = 'local_pvas_vas_raw_base'
+    # data_name = 'procas_pvas_vas_raw_base'
+    priors_name = 'priors_pvas_vas_raw_base'
+    if split_CC_and_MLO:
+        base_name += '_' + CC_or_MLO
+        data_name += '_' + CC_or_MLO
+        priors_name += '_' + CC_or_MLO
+    processed_priors_path = processed_dataset_path+priors_name+'.pth'
+    processed_dataset_path += data_name+'.pth'
