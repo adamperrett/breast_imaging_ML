81828a1a63eda9eb0d4c477d05b06c3d4f375b6e
adamgoodtime <adam.perrett@manchester.ac.uk>
2024-10-01 19:24:48 +0100

    only either remove priors or don't

--- training/clean_csf_run_data.sh
+++ training/clean_csf_run_data.sh
@@ -7,6 +7,7 @@ rm ../models/l*
 rm ../models/r*
 rm submit_optuna_regression_job.*
 rm submit_optuna_mosaic_job.*
+rm submit_optuna_medici_job.*
 echo ""
 echo "     remember to stop the job submitting script"
 echo ""
