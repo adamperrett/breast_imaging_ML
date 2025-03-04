54c9dfb353080f6b38e0dddda793c4181f3fedf4
adamgoodtime <adam.perrett@manchester.ac.uk>
2025-03-04 21:50:33 +0000

    f'in' print

--- training/optuna_recurrence_medici.py
+++ training/optuna_recurrence_medici.py
@@ -317,14 +317,14 @@ def regression_training(trial):
             torch.save(model.state_dict(), working_dir + '/../models/l_' + best_model_name)
         else:
             not_improved_loss += 1
-        val_rec_string = ["{}: {}".format(l, np.array(bta)) for bta, l in zip(best_val_l_auc, recurrence_mapping)]
-        test_rec_string = ["{}: {}".format(l, np.array(bta)) for bta, l in zip(best_test_l_auc, recurrence_mapping)]
+        val_rec_string = ["{}: {}".format(l, bta.cpu().numpy()) for bta, l in zip(best_val_l_auc, recurrence_mapping)]
+        test_rec_string = ["{}: {}".format(l, bta.cpu().numpy()) for bta, l in zip(best_test_l_auc, recurrence_mapping)]
         print(f"From best val loss at epoch {epoch - not_improved_loss}: "
               f"val loss: {best_val_loss:.4f} test loss {best_test_loss:.4f}"
               f"val auc: {torch.mean(best_val_l_auc):.4f} test auc {torch.mean(best_test_l_auc):.4f} "
               f"val rec auc: {val_rec_string} test rec auc {test_rec_string}")
-        val_rec_string = ["{}: {}".format(l, np.array(bta)) for bta, l in zip(best_val_auc, recurrence_mapping)]
-        test_rec_string = ["{}: {}".format(l, np.array(bta)) for bta, l in zip(best_test_auc, recurrence_mapping)]
+        val_rec_string = ["{}: {}".format(l, bta.cpu().numpy()) for bta, l in zip(best_val_auc, recurrence_mapping)]
+        test_rec_string = ["{}: {}".format(l, bta.cpu().numpy()) for bta, l in zip(best_test_auc, recurrence_mapping)]
         print(f"From best val AUC at epoch {epoch - not_improved_auc}: "
               f"val loss: {best_val_a_loss:.4f} test loss {best_test_a_loss:.4f} "
               f"val auc: {torch.mean(best_val_auc):.4f} test auc {torch.mean(best_test_auc):.4f} "
